c2ec2ced674fd90ee60f84bed2f4b38644181dbf
JoelWhittle <joelwhittle90@gmail.com>
2017-02-01 12:13:12 +0000

    Init commit part 1

---
+++ Game/PrefabLibrary.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PrefabLibrary : MonoBehaviour
+{
+
+    public static PrefabLibrary Instance ;
+    public GameObject TablePrefab;
+
+	public GameObject GameCamera;
+	public GameObject[] Tiles;
+	public GameObject[] Props;
+    //tmp
+    public GameObject[] UnitPrefabs;
+
+    void Start()
+    {
+        Instance = this;
+    }
+}
